ba54716e46b5cbac11afefffd9f8bef44f931202
Naishadh Parmar <naishadh_parmar@live.com>
2019-01-30 21:17:04 +0530

    Delete unnecessary files.

--- logs/generate_statsP2.sh
+++
@@ -1,8 +0,0 @@
-
-won=$(cat $1 | awk '{print $4}' | grep 1 | wc -l)
-
-total=$(cat $1 | awk '{print $4}' | grep -E '1|2' | wc -l)
-
-win_rate=$(bc <<< "scale=2;$won/$total")
-
-echo "Player 1 won: " $win_rate "% games"
